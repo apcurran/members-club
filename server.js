937fda1bf46cb70743ee37ee97ea4f5684091bad
apc <alexcurran.va@gmail.com>
2020-01-28 12:00:23 -0600

    Main server setup

--- server.js
+++ server.js
@@ -1,12 +1,40 @@
 const express = require("express");
 const app = express();
 const logger = require("morgan");
+const expressLayouts = require("express-ejs-layouts");
+const PORT = process.env.PORT || 3000;
+const path = require("path");
+
+const indexRouter = require("./routes/index");
 
 // View Engine Setup
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.set("layout", "layouts/layout");
 
 app.use(logger("dev"));
+app.use(expressLayouts);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Routers
+app.use("/", indexRouter);
+
+app.listen(PORT, console.log(`Server listening on port ${PORT}.`));
+
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//     next(createError(404));
+// });
+  
+// // error handler
+// app.use(function(err, req, res, next) {
+//     // set locals, only providing error in development
+//     res.locals.message = err.message;
+//     res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//     // render the error page
+//     res.status(err.status || 500);
+//     res.render('error');
+// });
