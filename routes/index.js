41c25e7389da13e68ae87bb78af3d07adcf59dbe
apc <alexcurran.va@gmail.com>
2020-01-30 14:30:17 -0600

    Added POST route to save new messages to db from index page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,45 @@
 const express = require("express");
 const router = express.Router();
 const { checkAuthenticated } = require("../config/checkAuth");
+const Message = require("../models/Message");
+const { validationResult } = require('express-validator'); // Validate data
+const { body } = require('express-validator');
 
 router.get("/", checkAuthenticated, (req, res) => {
     res.render("index", { title: "Members Only", user: req.user });
 });
+
+router.post("/", [
+
+    body("name")
+        .not().isEmpty()
+        .trim()
+        .escape(),
+    body("message")
+        .not().isEmpty()
+        .trim()
+        .escape()
+
+], async (req, res, next) => {
+    try {
+        const errors = validationResult(req);
+        // Re-render page with error message
+        if (!errors.isEmpty()) {
+            return res.render("index", { title: "Member's Only Messages", errors: errors.array() });
+        }
+
+        const message = new Message({
+            name: req.body.name,
+            message: req.body.message
+        });
+
+        await message.save();
+        res.redirect("/");
+
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+})
 
 module.exports = router;
