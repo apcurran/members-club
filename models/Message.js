d8dca123f5ccffafc8fd57c4db119eeaa425c8c6
apc <alexcurran.va@gmail.com>
2020-01-31 09:24:23 -0600

    Added virtual property for date formatting using Moment

--- models/Message.js
+++ models/Message.js
@@ -1,9 +1,16 @@
 const mongoose = require("mongoose");
+const moment = require("moment");
 
 const MessageSchema = new mongoose.Schema({
     name: { type: String, required: true, min: 1, max: 100 },
     message: { type: String, required: true, min: 1, max: 10000 },
     date: { type: Date, default: Date.now }
 });
+
+MessageSchema
+    .virtual("dateFormatted")
+    .get(function() {
+        return moment(this.date).format("MMMM Do, YYYY");
+    });
 
 module.exports = mongoose.model("Message", MessageSchema);
